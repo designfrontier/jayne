701b71e929997f0af0b5c14993d83143877a45ba
Daniel Sellers <daniel@designfrontier.net>
2015-07-07 08:34:26 -0600

    fixed the index.js file so it actually works right

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fetch from './modules/fetch';
 import xhr from './modules/xhr';
 
-window.dal = function (config) {
+window.jayne = function (config) {
     if(typeof window.fetch === 'function'){
         window.fetch = fetch(config);
     }
