1385f492ef2bae99c81b101f2d708d4f4910f6fd
Daniel Sellers <d.sellers@ldschurch.org>
2015-07-08 16:10:29 -0600

    work on the xhr object

--- modules/xhr.js
+++ modules/xhr.js
@@ -1,4 +1,5 @@
 let oldXHR = window.XMLHttpRequest
+    , config
     , xhr = function () {
             let tempReq = new oldXHR()
                 , request = {}
@@ -24,41 +25,66 @@ let oldXHR = window.XMLHttpRequest
             request.oldSend = request.send;
 
             request.send = function () {
-                if(typeof config.store === 'undefined' || config.store) {
-                    localforage.getItem(pathIn).then(function (doc) {
-                        if(doc === null){
-                            request.addEventListener('load', function (data) {
-                                if(typeof config.encrypt !== 'undefined' && config.encrypt){
-                                    localforage.setItem(pathIn, crypto.encrypt(request.response));
-                                } else {
-                                    localforage.setItem(pathIn, request.response);
-                                }
-                            });
 
-                            request.oldSend();
-                        } else {
-                            if(typeof config.encrypt !== 'undefined' && config.encrypt){
-                                request.response = crypto.decrypt(doc);
-                            } else {
-                                request.response = doc;
+                //first run through the request stack
+                config.request.reduce(function (prev, curr) {
+                    return prev.then(function (request) {
+                        return new Promise(function (resolve, reject) {
+                            var p = curr.apply({}, [{request: request, original: originalRequest}, resolve]);
+
+                            if(p instanceof Promise){
+                                p.then(function(resp){
+                                    resolve(resp);
+                                });
                             }
-                            request.status = 200;
-                            request.responseText = JSON.stringify(request.response);
-                            request.statusText = '200 OK';
-
-                            request.dispatchEvent(new Event('load'));
-                            request.onload();
-                        }
+                        });
                     });
-                } else {
-                    request.oldSend();
-                }
+
+                }, new Promise(function(res, rej){res({request: request, original: originalRequest})}))
+                    .then(function (request) {
+                        //check for request.response
+                        //  if it exists return it in the body
+                        //  if not then make the request
+
+                    })
+
+                //run through the response stack and trigger the
+                //  correct event when done
+
+
+                localforage.getItem(pathIn).then(function (doc) {
+                    if(doc === null){
+                        request.addEventListener('load', function (data) {
+                            if(typeof config.encrypt !== 'undefined' && config.encrypt){
+                                localforage.setItem(pathIn, crypto.encrypt(request.response));
+                            } else {
+                                localforage.setItem(pathIn, request.response);
+                            }
+                        });
+
+                        request.oldSend();
+                    } else {
+                        if(typeof config.encrypt !== 'undefined' && config.encrypt){
+                            request.response = crypto.decrypt(doc);
+                        } else {
+                            request.response = doc;
+                        }
+                        request.status = 200;
+                        request.responseText = JSON.stringify(request.response);
+                        request.statusText = '200 OK';
+
+                        request.dispatchEvent(new Event('load'));
+                        request.onload();
+                    }
+                });
             };
+
 
             return request;
         }
 
-        , configurator = function (config) {
+        , configurator = function (configObj) {
+            config = configObj
             return xhr;
         };
 
