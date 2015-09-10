b29714e759159f9c1f4525eed66b1cfc290d3c11
Cory Brown <oh.wise.man@gmail.com>
2015-09-10 14:04:22 -0600

    fetch wrapper was passing original fetch path and options, not result of middleware manipulations.

--- modules/fetch.js
+++ modules/fetch.js
@@ -4,7 +4,7 @@ let config
     , fetchAPI = window.fetch //the original fetch
 
     , fetch = function (pathIn, options) {
-        let promise = new Promise(function (resolve, reject){
+        let promise = new Promise( (resolve, reject) => {
             let fetchPromise
                 , requestStack = bernstein.create(config.request)
                 , responseStack = bernstein.create(config.response);
@@ -13,34 +13,28 @@ let config
                 //we have run through the request stack... now decide if we should
                 //  make the request by looking for
                 //  req.response
-                if(typeof req.response !== 'undefined'){
-                    //on to the resolve stack!
-                    fetchPromise = new Promise(function (res, rej) {
-                        res(new Response(JSON.stringify(req.response)));
-                    });
-                } else {
-                    fetchPromise = fetchAPI(pathIn, options);
-                }
+
+                fetchPromise = !req.response ?
+                    fetchAPI(req.path, req.options) :
+                    new Promise( (res, rej) => res(
+                        new Response(JSON.stringify(req.response)) )
+                    );
 
                 //now we need to run through the responseStack
                 //  get the data out of the Response object we will re-wrap it later
-                fetchPromise.then((fetchRes) => {
-                    fetchRes.json().then((data) => {
-                        responseStack(data).then((finishedData) => {
-                            //Rewrap the result in a response so that the external stuff can deal with it
-                            //  as a normal fetch response... yeah
-                            resolve(new Response(JSON.stringify(finishedData)));
-                        });
-                    }).catch((err) => {
-                        reject(err);
-                    });
-                }).catch((err) => {
-                    reject(err);
-                });
+                fetchPromise.then(
+                    (fetchRes) =>
+                        fetchRes.json().then( (data) =>
+                            responseStack(data).then( (finishedData) =>
+                                // Rewrap the result in a response so that the
+                                // external stuff can deal with it as a normal
+                                // fetch response... yeah
+                                resolve(new Response(JSON.stringify(finishedData)))
+                            )
+                        ).catch((err) => reject(err) )
+                ).catch((err) => reject(err));
 
-            }).catch((err) => {
-                reject(err);
-            });
+            }).catch( (err) => reject(err) );
         });
 
         return promise;
